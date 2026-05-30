f056f496200be5f78dc619c95b91c032244dd40c
Jan Stuhring <jan.stuehring.wmp1u@simplelogin.com>
2026-05-30 12:03:03 +0200

    removed unnecessary const overload

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -11,22 +11,4 @@ void iter(T* arr, const std::size_t size, void (*func)(T&))
   }
 }
 
-template <typename T>
-void iter(T* arr, const std::size_t size, void (*func)(const T&))
-{
-  for (std::size_t i = 0; i < size; ++i)
-  {
-    func(arr[i]);
-  }
-}
-
-template <typename T>
-void iter(const T* arr, const std::size_t size, void (*func)(const T&))
-{
-  for (std::size_t i = 0; i < size; ++i)
-  {
-    func(arr[i]);
-  }
-}
-
 #endif
